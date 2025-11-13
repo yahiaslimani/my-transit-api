1de63db9b64693af2ac938d38760f2399def7af1
Yahia Slimani <ey_slimani@esi.dz>
2025-11-13 22:22:27 +0100

    .

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // src/server.js
 require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
+const path = require('path');
 const { connectDB } = require('./config/database');
 const errorHandler = require('./middleware/errorHandler');
 const stopRoutes = require('./routes/stops');
@@ -16,6 +17,11 @@ app.use(express.json());
 // Connect to the database
 connectDB();
 
+
+const routePathsDir = path.join(__dirname, 'routePaths'); // __dirname is the directory of server.js (src)
+
+// Serve static files from the routePaths directory under the /api/kml route
+app.use('/api/kml', express.static(routePathsDir));
 // Define routes
 app.use('/api/stops', stopRoutes);
 app.use('/api/lines', lineRoutes);
